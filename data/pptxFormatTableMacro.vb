366891ab4d826b61e7ac7ae09f24c288a2699ac8
lelandjmurrin <lelandjmurrin@gmail.com>
2024-05-28 16:10:42 -0400

    Adding powerpoint macros for formatting data tables. Using pptx library for generating powerpoint tables directly from python

---
+++ data/pptxFormatTableMacro.vb
@@ -0,0 +1,74 @@
+Sub tidytable()
+    With Application.ActiveWindow.View.Slide.Shapes
+        For i = 1 To .Count
+            If .Item(i).HasTable Then
+                Set Table = .Item(i).Table
+                ' Table.Cell(1, 1).Shape.TextFrame.TextRange.Text = "Cell 1"
+                ' Table.Cell(1, 1).Shape.TextFrame.TextRange.Font.Color = RGB(0, 0, 0)
+                ' Table.Cell(1, 1).Shape.TextFrame.TextRange.Font.Size = 20
+                ' Table.Cell(1, 1).Shape.TextFrame.TextRange.Font.Name = "Arial"
+                For c = 1 To Table.Columns.Count
+                    For r = 1 To Table.Rows.Count
+                        With Table.Rows(r).Cells(c)
+                            .Borders.Item(ppBorderBottom).ForeColor.RGB = RGB(0, 0, 0)
+                            .Borders.Item(ppBorderTop).ForeColor.RGB = RGB(0, 0, 0)
+                            .Borders.Item(ppBorderRight).ForeColor.RGB = RGB(0, 0, 0)
+                            .Borders.Item(ppBorderLeft).ForeColor.RGB = RGB(0, 0, 0)
+                            .Borders.Item(ppBorderBottom).Weight = 1
+                            .Borders.Item(ppBorderRight).Weight = 1
+                            .Shape.Fill.ForeColor.RGB = RGB(235, 241, 233)
+                            If r = 1 Or c = 1 Then
+                                .Shape.Fill.ForeColor.RGB = RGB(112, 173, 71)
+                            End If
+                            If r = 1 Then
+                                .Borders.Item(ppBorderTop).Weight = 1
+                                .Borders.Item(ppBorderBottom).Weight = 3
+                            End If
+                            If c = 1 Then
+                                .Borders.Item(ppBorderLeft).Weight = 1
+                                .Borders.Item(ppBorderRight).Weight = 3
+                            End If
+                        End With
+                    Next
+                Next
+            End If
+        Next
+    End With
+End Sub
+
+
+
+Sub tidytable_noidx()
+    With Application.ActiveWindow.View.Slide.Shapes
+        For i = 1 To .Count
+            If .Item(i).HasTable Then
+                Set Table = .Item(i).Table
+                For c = 1 To Table.Columns.Count
+                    For r = 1 To Table.Rows.Count
+                        With Table.Rows(r).Cells(c)
+                            .Borders.Item(ppBorderBottom).ForeColor.RGB = RGB(0, 0, 0)
+                            .Borders.Item(ppBorderTop).ForeColor.RGB = RGB(0, 0, 0)
+                            .Borders.Item(ppBorderRight).ForeColor.RGB = RGB(0, 0, 0)
+                            .Borders.Item(ppBorderLeft).ForeColor.RGB = RGB(0, 0, 0)
+                            .Borders.Item(ppBorderBottom).Weight = 1
+                            .Borders.Item(ppBorderRight).Weight = 1
+                            .Shape.Fill.ForeColor.RGB = RGB(235, 241, 233)
+                            Debug.Print "apple"
+                            If r = 1 Then
+                                .Shape.Fill.ForeColor.RGB = RGB(112, 173, 71)
+                            End If
+                            If r = 1 Then
+                                .Borders.Item(ppBorderTop).Weight = 1
+                                .Borders.Item(ppBorderBottom).Weight = 3
+                            End If
+'                            If c = 1 Then
+'                                .Borders.Item(ppBorderLeft).Weight = 1
+'                                .Borders.Item(ppBorderRight).Weight = 3
+'                            End If
+                        End With
+                    Next
+                Next
+            End If
+        Next
+    End With
+End Sub
