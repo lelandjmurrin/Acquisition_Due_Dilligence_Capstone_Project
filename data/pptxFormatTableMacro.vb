97efaa036fa23a0057e0eea52ea6db403a09381f
lelandjmurrin <lelandjmurrin@gmail.com>
2024-06-08 21:29:24 -0400

    Added function that creates tables directly in powerpoint to dynamically upload to presentation

--- data/pptxFormatTableMacro.vb
+++ data/pptxFormatTableMacro.vb
@@ -17,6 +17,7 @@ Sub tidytable()
                             .Borders.Item(ppBorderBottom).Weight = 1
                             .Borders.Item(ppBorderRight).Weight = 1
                             .Shape.Fill.ForeColor.RGB = RGB(235, 241, 233)
+                            .Shape.TextFrame.TextRange.Font.Color = RGB(0, 0, 0)
                             If r = 1 Or c = 1 Then
                                 .Shape.Fill.ForeColor.RGB = RGB(112, 173, 71)
                             End If
@@ -43,8 +44,15 @@ Sub tidytable_noidx()
         For i = 1 To .Count
             If .Item(i).HasTable Then
                 Set Table = .Item(i).Table
+                With Table.Rows(1).Cells(1)
+                    .Shape.Fill.Transparency = 1
+                    .Shape.TextFrame.TextRange.Font.Color = RGB(0, 0, 0)
+                    .Borders.Item(ppBorderTop).ForeColor.RGB = RGB(255, 255, 255)
+                    .Borders.Item(ppBorderLeft).ForeColor.RGB = RGB(255, 255, 255)
+                    .Borders.Item(ppBorderRight).ForeColor.RGB = RGB(255, 255, 255)
+                End With
                 For c = 1 To Table.Columns.Count
-                    For r = 1 To Table.Rows.Count
+                    For r = 2 To Table.Rows.Count
                         With Table.Rows(r).Cells(c)
                             .Borders.Item(ppBorderBottom).ForeColor.RGB = RGB(0, 0, 0)
                             .Borders.Item(ppBorderTop).ForeColor.RGB = RGB(0, 0, 0)
@@ -53,11 +61,9 @@ Sub tidytable_noidx()
                             .Borders.Item(ppBorderBottom).Weight = 1
                             .Borders.Item(ppBorderRight).Weight = 1
                             .Shape.Fill.ForeColor.RGB = RGB(235, 241, 233)
-                            Debug.Print "apple"
-                            If r = 1 Then
+                            .Shape.TextFrame.TextRange.Font.Color = RGB(0, 0, 0)
+                            If r = 2 Then
                                 .Shape.Fill.ForeColor.RGB = RGB(112, 173, 71)
-                            End If
-                            If r = 1 Then
                                 .Borders.Item(ppBorderTop).Weight = 1
                                 .Borders.Item(ppBorderBottom).Weight = 3
                             End If
